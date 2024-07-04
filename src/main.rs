9e64ae789f4a3367b1368aba0fd20da52a019cda
kuuuube <hexagonisalie@gmail.com>
2024-07-04 13:43:53 -0400

    Basic prototype

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
 fn main() {
-    let source_image = image::open("source.png").unwrap().as_rgb8().unwrap().to_owned();
-
-    let (source_width, source_height) = (source_image.width(), source_image.height());
-
-    let mut output_image: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = image::ImageBuffer::new(source_width, source_height);
-
-    for (x, y, rgb) in source_image.enumerate_pixels() {
-        let (unit_x, unit_y) = ((x + (source_width / 2)) as f32 / source_width as f32, (y + (source_height / 2)) as f32 / source_height as f32);
-        let (transform_x, transform_y) = (unit_x / 2.0, unit_y / 2.0);
-        let (real_x, real_y) = ((transform_x * source_width as f32) as u32, (transform_y * source_height as f32) as u32);
-
-        let output_pixel = output_image.get_pixel_mut(real_x, real_y);
-        *output_pixel = image::Rgb([*rgb.0.get(0).unwrap(), *rgb.0.get(1).unwrap(), *rgb.0.get(2).unwrap()]);
-    }
+    let source_image = image::open("source.png").unwrap();
+    let output_image = process_image(source_image);
 
     output_image.save("output.png").unwrap();
+}
+
+fn transform(pixel_coords: Point) -> Point {
+    return pixel_coords;
+}
+
+fn process_image(image: image::DynamicImage) -> image::ImageBuffer<image::Rgba<u8>, Vec<u8>> {
+    let source_image = image.into_rgba8();
+    let source_size = Size {
+        width: source_image.width(),
+        height: source_image.height(),
+    };
+
+    let output_size = Size {
+        width: 1000,
+        height: 1000,
+    };
+    let mut output_image: image::ImageBuffer<image::Rgba<u8>, Vec<u8>> =
+        image::ImageBuffer::new(output_size.width, output_size.height);
+
+    for (x, y, rgba) in source_image.enumerate_pixels() {
+        let unit_point = Point {
+            x: x as f64 / source_size.width as f64 * 2.0 - 1.0,
+            y: y as f64 / source_size.height as f64 * 2.0 - 1.0,
+        };
+        let transformed_point = transform(unit_point);
+
+        if transformed_point.x > 1.0
+            || transformed_point.x < -1.0
+            || transformed_point.y > 1.0
+            || transformed_point.y < -1.0
+        {
+            continue;
+        }
+
+        let (real_x, real_y) = (
+            ((transformed_point.x + 1.0) / 2.0 * output_size.width as f64) as u32,
+            ((transformed_point.x + 1.0) / 2.0 * output_size.height as f64) as u32,
+        );
+
+        let output_pixel = output_image.get_pixel_mut(real_x, real_y);
+        *output_pixel = rgba.clone();
+    }
+    return output_image;
+}
+
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+struct Size {
+    width: u32,
+    height: u32,
 }
