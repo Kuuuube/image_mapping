de8f53a6481fa558b8eb1ea6c770488ef26e1f54
kuuuube <hexagonisalie@gmail.com>
2024-07-04 16:04:52 -0400

    Add Elliptical Grid Mapping Inverse as example

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ fn main() {
     let source_image = image::open("source.png").unwrap();
     let output_image = transformer::transform_image(
         source_image,
-        Some(transformer::Size {
-            width: 1000,
-            height: 1000,
-        }),
-        |point| transformer::Point {
-            x: point.x / 2.0,
-            y: point.y / 2.0,
-        },
+        None,
+        |point| {
+            // Elliptical Grid Mapping Inverse (Square -> Circle)
+            transformer::Point {
+                x: point.x * f64::sqrt(1.0 - (f64::powi(point.y, 2) / 2.0)),
+                y: point.y * f64::sqrt(1.0 - (f64::powi(point.x, 2) / 2.0)),
+            }
+        }
     );
 
     output_image.save("output.png").unwrap();
