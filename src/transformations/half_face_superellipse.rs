c9ca9fdf77e16e161ef797842c6f862e4261e17c
kuuuube <hexagonisalie@gmail.com>
2024-07-04 22:00:04 -0400

    Move superellipse function to wrapper

--- src/transformations/half_face_superellipse.rs
+++ src/transformations/half_face_superellipse.rs
@@ -1,5 +1,30 @@
 use crate::transformer::{self, Point};
 
+fn superellipse_wrapper<F>(point: transformer::Point, mut transformation: F) -> Point
+where
+    F: FnMut(Point) -> Point,
+{
+    let transformed_point = transformation(point);
+    let expanded_point = Point {
+        x: transformed_point.x * 1.41421356237,
+        y: transformed_point.y * 1.41421356237,
+    };
+    if expanded_point.x > 1.0
+        || expanded_point.x < -1.0
+        || expanded_point.y > 1.0
+        || expanded_point.y < -1.0
+    {
+        return transformer::Point {
+            x: f64::INFINITY,
+            y: f64::INFINITY,
+        };
+    }
+    return transformer::Point {
+        x: point.x,
+        y: point.y,
+    };
+}
+
 //FG-Squircular
 
 //FG-Squircular Secondary
@@ -10,22 +35,7 @@ use crate::transformer::{self, Point};
 
 //Elliptical Grid
 pub fn elliptical_grid(point: transformer::Point) -> transformer::Point {
-    let transformed_point = super::square_to_circle::elliptical_grid(point);
-    let expanded_point = Point {
-        x: transformed_point.x * 1.41421356237,
-        y: transformed_point.y * 1.41421356237,
-    };
-    if expanded_point.x > 1.0
-        || expanded_point.x < -1.0
-        || expanded_point.y > 1.0
-        || expanded_point.y < -1.0
-    {
-        return transformer::Point { x: 200.0, y: 200.0 };
-    }
-    return transformer::Point {
-        x: point.x,
-        y: point.y,
-    };
+    return superellipse_wrapper(point, super::square_to_circle::elliptical_grid);
 }
 
 //Elliptical Grid Secondary
