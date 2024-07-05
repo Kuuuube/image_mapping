983d4492a708f9a570a00eab3b20ded2236558c2
kuuuube <hexagonisalie@gmail.com>
2024-07-04 21:02:32 -0400

    Split batch running out of main

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,20 @@ fn main() {
     let start_time = std::time::Instant::now();
 
     let source_image = image::open("source.png").unwrap();
-    let batch_size = transformer::Size {
-        width: 1000,
-        height: 1000,
-    };
-    batch_runners::run_all_circle_to_square(source_image.clone(), Some(batch_size));
-    batch_runners::run_all_square_to_circle(source_image.clone(), Some(batch_size));
+
+    run_batches(
+        source_image,
+        transformer::Size {
+            width: 2000,
+            height: 2000,
+        },
+    );
 
     println!("Generated in: {}ms", start_time.elapsed().as_millis());
+}
+
+fn run_batches(source_image: image::DynamicImage, batch_size: transformer::Size) {
+    batch_runners::run_all_circle_to_square(source_image.clone(), Some(batch_size));
+    batch_runners::run_all_square_to_circle(source_image.clone(), Some(batch_size));
+    batch_runners::run_all_half_face_superellipse(source_image.clone(), Some(batch_size));
 }
